0796035653328cccdb698341c31651138ad586a7
Annette Musick <Annette.Musick@zaplabs.com>
2018-02-06 11:59:51 -0800

    case statements another way

--- luser-demo09.sh
+++ luser-demo09.sh
@@ -16,17 +16,27 @@
 #   exit 1
 # fi
 
+# case "${1}" in
+#   start)
+#     echo 'Starting.'
+#     ;;
+#   stop)
+#     echo 'Stopping.'
+#     ;;
+#   status|state|--status|--state)
+#     echo 'Status:'
+#     ;;
+#   *)
+#     echo 'Supply a valid option.' >&2
+#     exit 1
+#     ;;
+# esac
+
 case "${1}" in
-  start)
-    echo 'Starting.'
-    ;;
-  stop)
-    echo 'Stopping.'
-    ;;
-  status|state)
-    echo 'Status:'
-    ;;
-   *)
+  start) echo 'Starting.' ;;
+  stop) echo 'Stopping.' ;;
+  status) echo 'Status:' ;;
+  *)
     echo 'Supply a valid option.' >&2
     exit 1
     ;;
